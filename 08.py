ff26e129afa24f0aa048edd195b26e2d726a722d
Dao Heng Liu <daohengliu@gmail.com>
2023-12-08 06:55:44 +0000

    day 8: part 2 that doesn't take days to run

--- 08.py
+++ 08.py
@@ -1,5 +1,5 @@
+from functools import reduce
 from typing import List
-import tqdm
 from line_profiler_pycharm import profile
 
 from input_reader import read_input_from_file
@@ -7,8 +7,6 @@ from input_reader import read_input_from_file
 
 def part1(input_lines: List[str]):
     left_right, node_map = parse_input(input_lines)
-    print(left_right)
-    print(node_map)
 
     current_node = "AAA"
     steps = 0
@@ -19,36 +17,42 @@ def part1(input_lines: List[str]):
     print(f"part 1: done in {steps} steps")
 
 
-def main(input_lines: List[str]):
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+def lcm_many(*numbers):
+    return reduce(lcm, numbers)
+
+
+@profile
+def part2(input_lines: List[str]):
     left_right, node_map = parse_input(input_lines)
 
-    # part 2
-    loop_map = dict()
     nodes = [node for node in node_map.keys() if node.endswith("A")]
-    pbar = tqdm.tqdm()
+    simultaneous_big_steps = []
+    for node in nodes:
+        current_node = node
+        steps = 0
+        big_steps = 0
+        while not current_node.endswith("Z"):
+            for direction in left_right:
+                current_node = node_map[current_node][0 if direction == "L" else 1]
+                steps += 1
+                if current_node.endswith("A"):
+                    print(f"after {steps} steps, node {node} arrived at {current_node}")
+            big_steps += 1
+        # print(f"{node}: done in {steps} steps ({big_steps} big steps) | "
+        #       f"{big_steps}*{len(left_right)} = {big_steps * len(left_right)}")
+        simultaneous_big_steps.append(big_steps)
 
-    def advance_single_node(node: str) -> str:
-        start_node = node
-        for d in left_right:
-            node = node_map[node][d == "R"]
-        loop_map[start_node] = node
-        return node
-
-    lr_length = len(left_right)
-    big_step = 0
-    while True:
-        nodes = [loop_map[node] if node in loop_map else advance_single_node(node) for node in nodes]
-        pbar.update(lr_length)
-
-        do_break = True
-        for node in nodes:
-            if not node.endswith("Z"):
-                do_break = False
-                break
-        if do_break:
-            break
-        big_step += 1
-    print(f"done in {big_step * lr_length} steps")
+    print(f"part 2: done in {lcm_many(*simultaneous_big_steps) * len(left_right)} steps")
 
 
 def parse_input(input_lines):
@@ -64,4 +68,4 @@ def parse_input(input_lines):
 if __name__ == '__main__':
     puzzle_input = read_input_from_file("08.txt")
     part1(puzzle_input)
-    main(puzzle_input)
+    part2(puzzle_input)
