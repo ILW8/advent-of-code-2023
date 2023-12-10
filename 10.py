6005cd96eea88828ea1f2feff65a8c605a2faa7c
Dao Heng Liu <daohengliu@gmail.com>
2023-12-10 10:55:59 +0000

    day 10: part 2

--- 10.py
+++ 10.py
@@ -1,14 +1,14 @@
+import functools
 from typing import List
 from input_reader import read_input_from_file
 import numpy as np
 from termcolor import colored
-
+from matplotlib import pyplot as plt
 
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
-
 
 PIPE_TYPES = {
     '|': [(0, 1), (0, -1)],
@@ -18,6 +18,32 @@ PIPE_TYPES = {
     '7': [(-1, 0), (0, -1)],
     'F': [(0, -1), (1, 0)],
     '.': None
+}
+PIPE_NP_TYPES = {
+    '|': np.array([[0, 1, 0],
+                   [0, 1, 0],
+                   [0, 1, 0]]),
+    '-': np.array([[0, 0, 0],
+                   [1, 1, 1],
+                   [0, 0, 0]]),
+    'L': np.array([[0, 1, 0],
+                   [0, 1, 1],
+                   [0, 0, 0]]),
+    'J': np.array([[0, 1, 0],
+                   [1, 1, 0],
+                   [0, 0, 0]]),
+    '7': np.array([[0, 0, 0],
+                   [1, 1, 0],
+                   [0, 1, 0]]),
+    'F': np.array([[0, 0, 0],
+                   [0, 1, 1],
+                   [0, 1, 0]]),
+    'S': np.array([[0, 2, 0],
+                   [2, 2, 2],
+                   [0, 2, 0]]),
+    '.': np.array([[0, 0, 0],
+                   [0, 0, 0],
+                   [0, 0, 0]]),
 }
 
 
@@ -102,8 +128,7 @@ def pretty_print_pipes(input_lines: List[str], use_colors=False):
         line = line.replace("-", "━")
         line = line.replace("|", "┃")
         line = line.replace(".", " ")
-        # print(colored(line, "white", "on_grey", force_color=True))
-        # print(colored(line, "white", "on_dark_grey", force_color=True))
+
         if use_colors:
             grey = idx % 2 == 1
             for character in line:
@@ -118,9 +143,9 @@ def pretty_print_pipes(input_lines: List[str], use_colors=False):
 
 
 def filter_main_loop(input_lines: List[str], distance_map: np.array):
-    main_loop_bitmap = loop_distance_map >= 0
+    main_loop_bitmap = distance_map >= 0
     filtered_input = []
-    for row, line in enumerate(puzzle_input):
+    for row, line in enumerate(input_lines):
         a = np.array(list(line))
         a[~main_loop_bitmap[row]] = '.'
         filtered_input.append(''.join(a))
@@ -128,13 +153,53 @@ def filter_main_loop(input_lines: List[str], distance_map: np.array):
 
 
 def part2(input_lines: List[str]):
-    pass
+    bigger_map = np.zeros((len(input_lines[0]) * 3, len(input_lines) * 3))
+
+    for row_index, row in enumerate(input_lines):
+        for col_index, col in enumerate(row):
+            bigger_map[row_index * 3:row_index * 3 + 3, col_index * 3:col_index * 3 + 3] = PIPE_NP_TYPES[col]
+
+    # BFS tiles outside
+    visited = {(0, 0)}
+    queue = [(0, 0)]
+    while queue:
+        current = queue.pop(0)
+
+        for direction in [UP, DOWN, LEFT, RIGHT]:
+            new_x, new_y = current[0] + direction[0], current[1] + direction[1]
+            neighbour = (new_x, new_y)
+
+            if (new_x < 0
+                    or new_y < 0
+                    or new_x >= bigger_map.shape[1]
+                    or new_y >= bigger_map.shape[0]
+                    or neighbour in visited
+                    or bigger_map[new_y][new_x] != 0):
+                continue
+
+            visited.add(neighbour)
+            queue.append(neighbour)
+            bigger_map[new_y][new_x] = 5
+    bigger_map[bigger_map == 0] = -5
+    # plt.imshow(bigger_map, interpolation='nearest')
+    # plt.show()
+
+    inner_count = 0
+    for row_index, row in enumerate(input_lines):
+        for col_index, col in enumerate(row):
+            inner_count += bigger_map[1 + row_index * 3, 1 + col_index * 3] == -5
+    print(inner_count)
 
 
 if __name__ == '__main__':
     use_sample = False
     puzzle_input = read_input_from_file("10_sample.txt" if use_sample else "10.txt")
-    loop_distance_map = part1(puzzle_input)
-    part2(puzzle_input)
 
-    pretty_print_pipes(filter_main_loop(puzzle_input, loop_distance_map))
+    # part 1
+    loop_distance_map = part1(puzzle_input)
+
+    # part 2
+    filtered_main_loop = filter_main_loop(puzzle_input, loop_distance_map)
+    pretty_print_pipes(filtered_main_loop)
+    part2(filtered_main_loop)
+
